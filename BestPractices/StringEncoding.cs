4a843d21f8f7f00d687f92809c2927d02f0ec068
Antony Nelson G <antony.nelsong@siemens-healthineers.com>
2022-08-19 09:50:56 +0530

    add projects

---
+++ BestPractices/StringEncoding.cs
@@ -0,0 +1,7 @@
+﻿namespace BestPractices
+{
+    public class StringEncoding
+    {
+
+    }
+}
---
+++ BestPracticesTests/StringEncodingTests.cs
@@ -0,0 +1,16 @@
+namespace BestPracticesTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
